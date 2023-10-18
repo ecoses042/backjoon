b0150282092b13b4459e5c4a92f4a841490dc0a5
ecoses042 <ecoses042@gmail.com>
2023-10-18 18:31:02 +0900

    3rd try

--- backjoon10773.py
+++
@@ -1,9 +0,0 @@
-K = int(input())
-nums = []
-for i in range(K):
-	num = int(input())
-	if num == 0:
-		nums.pop()
-	else:
-		nums.append(num)
-print(sum(nums))
--- backjoon15667.py
+++
@@ -1,8 +0,0 @@
-import math
-
-n = int(input())
-n -= 1
-for k in range(1, n + 1):
-	if n == k + k ** 2:
-		print(k)
-		break
